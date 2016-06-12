515092b0851e44eeccc64e40826130d6028adf33
AmazingPoint <greenpointan@icloud.com>
2016-06-12 14:14:25 +0800

    fix osx10.11+ install bug

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ echo 'copy trans.py to /usr/local/share'
 sudo cp trans.py /usr/local/share
 echo 'add execute permission '
 sudo chmod +x trans.py
-echo 'lin to path /usr/bin'
-sudo ln -s /usr/local/share/trans.py /usr/bin/trans
+echo 'lin to path /usr/local/bin'
+sudo ln -s /usr/local/share/trans.py /usr/local/bin/trans
 echo 'SUCCESSFUL'
 echo 'You can use trans'
